1131b4b86cc4f09ab5b75853068d5d8b97c78478
unknown <ayondevs.94b@gmail.com>
2026-04-22 17:03:15 +0530

    Setting up HeroUI in Next.js

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@heroui/react";
+
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero page</h2>
+            <Button variant="secondary">Hero Button</Button>
+            <Button variant="outline">Hero Button</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
